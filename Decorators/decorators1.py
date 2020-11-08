085d64a81ceb60a6accb601015057182f434820d
Rohan Sharma <rohan.sharma@o9solutions.com>
2020-11-08 23:43:01 +0530

    New File added for the decoratots concept

--- Decorators/decorators1.py
+++ Decorators/decorators1.py
@@ -6,4 +6,13 @@ def div(a,b):
     print(a/b)
 
 
+def smartDiv(func):
+    def innerDiv(a,b):
+        if a < b:
+            a,b = b,a # reverse the numbers
+        return func(a,b)
+    return innerDiv
+
+div = smartDiv(div)
+
 div(2,4)
