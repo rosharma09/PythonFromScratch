26e529a988af0d770c40822e0f8dce8c06b82a73
rohan.sharma <sharmarahan123@gmail.com>
2020-11-10 20:59:22 +0530

    New file for decorator

--- Decorators/decorators2.py
+++ Decorators/decorators2.py
@@ -1,3 +1,30 @@
 # this is a sample program to illustrate the usage of decorators
 
+# define a function which calculates the difference of two passed values
+
+def difference(a,b):
+    return a - b
+
+# in case the first argument is greater than the second argument the difference comes in negative
+
+# define a decorator which will return the difference in +ve
+
+def differencePositive(func):
+    def innerFunction(a,b):
+        if a < b:
+            # logic to swap the numbers
+            temp = a
+            a = b
+            b = temp
+        return func(a,b)
+    return innerFunction
+
+
+# create a relation between the main function and decorator
+
+difference = differencePositive(difference)
+
+res = difference(10,20)
+print('The difference is {}'.format(res))
+
 
